28b915b93ef19564d3caa53d8f28e4ae432021ce
RustyApe <110228309+Rusty-Ape@users.noreply.github.com>
2022-11-05 20:58:16 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -37,10 +37,80 @@ app.post('/arithmetric',(req,res)=>{
             res.json(result) 
             break
         default:
-            res.json(null)
-    }
+            if (operator == null){
+                res.json(null)
+            }
+            else{
+                var action = regx1(operator.toLowerCase())
+                if (action == null){
+                    res.json(null)
+                    return null
+                }
+                var num_obj = regx_num(operator)
+
+                if (num_obj == null) {
+                    res.json(null)
+                    return null
+                }
+
+                if (num_obj[0]== null || num_obj[1]==null){
+                    res.json(null)
+                    return null
+                }
+                var a = parseInt(num_obj[0])
+                var b = parseInt(num_obj[1])
+            }
+            switch(action){
+                case "addition":
+                   var eval = a + b
+                   var result = {"slackUsername":"rusty","operation_type":operator,"result":eval}
+                   res.json(result) 
+                   break
+                case "add":
+                   var eval = a + b
+                   var result = {"slackUsername":"rusty","operation_type":operator,"result":eval}
+                   res.json(result) 
+                   break
+                case "subtraction":
+                    var eval = a-b
+                    var result = {"slackUsername":"rusty","operation_type":operator,"result":eval}
+                    res.json(result) 
+                   break
+                case "subtract":
+                    var eval = a-b
+                    var result = {"slackUsername":"rusty","operation_type":operator,"result":eval}
+                    res.json(result) 
+                   break
+                case "multiplication":
+                    var eval = a *b
+                    var result = {"slackUsername":"rusty","operation_type":operator,"result":eval}
+                    res.json(result) 
+                    break
+                case "multiply":
+                    var eval = a *b
+                    var result = {"slackUsername":"rusty","operation_type":operator,"result":eval}
+                    res.json(result) 
+                    break
+                default:
+                    res.json(null)
+            }
+    }   
 })
 
 app.listen(PORT,()=>{
     console.log("Server has started running on port",PORT)
 })
+
+function regx1(operator){
+    const re1 = /(\badd\b|\baddition\b|\bsubtraction\b|\bsubtract\b|\bmultiply\b|\bmultiplication\b)/
+    match_result=operator.match(re1)
+    if (match_result == null) return null
+    return match_result[0]
+}
+
+function regx_num(operator){
+    const re = /[+-]?\d+/g
+    match_result=operator.match(re)
+    if (match_result == null) return null
+    return match_result
+}

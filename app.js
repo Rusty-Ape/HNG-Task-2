445ad773ea5fc359f4847eaec53e18a6c403b59f
aise <aiseluck@gmail.com>
2022-11-05 10:00:15 +0100

    HNG task 2

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors")
 const app = express();
 const PORT = process.env.PORT || 4321;
 
+app.use(express.json())
+
 app.use(cors({
     origin: '*'
 }));
@@ -13,6 +15,30 @@ app.get('/',(req,res)=>{
 
 app.get('/myself',(req,res)=>{
     res.json({"slackUsername":"aisosa.arasomwan","backend":true,"age":24,"bio":"An Engineer who likes NodeJs"})
+})
+
+app.post('/arithmetric',(req,res)=>{
+    var body = req.body
+    var operator = body.operation_type
+    switch(operator){
+        case "addition":
+           var eval = body.x + body.y
+           var result = {"slackUsername":"rusty","operation_type":operator,"result":eval}
+           res.json(result) 
+           break
+        case "subtraction":
+            var eval = body.x-body.y
+            var result = {"slackUsername":"rusty","operation_type":operator,"result":eval}
+            res.json(result) 
+           break
+        case "multiplication":
+            var eval = body.x * body.y
+            var result = {"slackUsername":"rusty","operation_type":operator,"result":eval}
+            res.json(result) 
+            break
+        default:
+            res.json(null)
+    }
 })
 
 app.listen(PORT,()=>{
